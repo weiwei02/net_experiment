61aba85c0207d6625743e813f9681304c02219c2
weiwei3.wang <weiwei3.wang@ly.com>
2020-11-25 07:57:57 +0000

    add tokio echo

---
+++ rnet/src/bin/tokio_proxy.rs
@@ -0,0 +1,9 @@
+use std::error::Error;
+
+/**
+ * a proxy that forwards data to another server and forwards that server's
+ * response back to clients. 
+*/
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {}
